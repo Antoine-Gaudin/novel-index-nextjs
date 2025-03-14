b4dd8ba0042e299efbaedc86561ed5fd2a5b9d9a
gaudin <agaudin76@gmail.com>
2025-03-14 09:43:40 +0100

    correctif du transfert

--- src/app/api/import-oeuvres/route.js
+++ src/app/api/import-oeuvres/route.js
@@ -6,13 +6,12 @@ export async function POST(req) {
         return new Response(JSON.stringify({ error: "Format JSON incorrect" }), { status: 400 });
       }
   
-      const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
-      const STRAPI_TOKEN = process.env.STRAPI_ADMIN_TOKEN;
+      const STRAPI_TOKEN = "c356ed5764de60851fd2aa5403f27e3ea359c71623bcdf01ab77bdd3d10728b5bac448a3f7b5a64bcafac7d8789bbc37526f60ca149401df54f1ead0f9573786434109e8b4da06cab1fb3b84cb8a8c21da7c3e6bce8d50fe00ea426bdcccbb3e274c62d04a6a6cf3fba8ebba10e599f5c20d4424bb8e139ff94d4ee857a61a2c";
   
       const results = [];
   
       for (const oeuvre of oeuvres) {
-        const response = await fetch(`${API_URL}/api/oeuvres`, {
+        const response = await fetch(`https://novel-index-strapi.onrender.com/api/oeuvres`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
